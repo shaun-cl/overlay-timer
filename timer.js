0f7d341045b6ece1930c77b239d472b64e2ffcfb
Shaun Clowes <shaun.clowes@gmail.com>
2019-12-07 17:17:49 -0800

    Cleanups

--- timer.js
+++ timer.js
@@ -1,90 +1,92 @@
-function makeClockOverlayDiv(appendTo) {
-    var node = document.createElement("div");
-    appendTo = appendTo || document.body;
-    node.className = 'overlayTimer';
-    node.style.position = 'fixed';
-    node.style.top = '10px'; 
-    node.style.right = '400px'; 
-    node.style.borderStyle = 'solid';
-    node.style.borderWidth = '2px';
-    node.style.borderColor = 'black';
-    node.style.backgroundColor = 'white';
-    node.style.padding = '10px';
-    node.innerHTML = '<div style="display: inline-block"><span class="hours"></span></div> : ' + 
-                     '<div style="display: inline-block"><span class="minutes"></span></div> : ' + 
-                     '<div style="display: inline-block"><span class="seconds"></span></div>';
+(function () {
+    function makeClockOverlayDiv(appendTo) {
+        var node = document.createElement("div");
+        appendTo = appendTo || document.body;
+        node.className = 'overlayTimer';
+        node.style.position = 'fixed';
+        node.style.top = '10px'; 
+        node.style.right = '400px'; 
+        node.style.borderStyle = 'solid';
+        node.style.borderWidth = '2px';
+        node.style.borderColor = 'black';
+        node.style.backgroundColor = 'white';
+        node.style.padding = '10px';
+        node.innerHTML = '<div style="display: inline-block"><span class="hours"></span></div> : ' + 
+                         '<div style="display: inline-block"><span class="minutes"></span></div> : ' + 
+                         '<div style="display: inline-block"><span class="seconds"></span></div>';
 
-    // Technically higher than the max signed positive integer but seems to work somehow
-    node.style.zIndex = '2147483647';
+        // Technically higher than the max signed positive integer but seems to work somehow
+        node.style.zIndex = '2147483647';
 
-    // Remove old nodes attached to this parent 
-    deleteChildClocks(appendTo);
+        // Remove old nodes attached to this parent 
+        deleteChildClocks(appendTo);
 
-    //document.body.insertBefore(node, document.body.firstChild);
-    appendTo.appendChild(node);
-    return node;
-}
+        //document.body.insertBefore(node, document.body.firstChild);
+        appendTo.appendChild(node);
+        return node;
+    }
 
-function deleteChildClocks(parentNode) {
-    Array.from(parentNode.children).filter(n => n.classList.contains("overlayTimer")).forEach(n => n.parentNode.removeChild(n));
-}
+    function deleteChildClocks(parentNode) {
+        Array.from(parentNode.children).filter(n => n.classList.contains("overlayTimer")).forEach(n => n.parentNode.removeChild(n));
+    }
 
-function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    return {
-        'total': t,
-        'days': days,
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
-    };
-}
+    function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
 
-function initializeClock(node, endtime) {
-    var clock = node;
-    var daysSpan = clock.querySelector('.days');
-    var hoursSpan = clock.querySelector('.hours');
-    var minutesSpan = clock.querySelector('.minutes');
-    var secondsSpan = clock.querySelector('.seconds');
+    function initializeClock(node, endtime) {
+        var clock = node;
+        var daysSpan = clock.querySelector('.days');
+        var hoursSpan = clock.querySelector('.hours');
+        var minutesSpan = clock.querySelector('.minutes');
+        var secondsSpan = clock.querySelector('.seconds');
 
-    function updateClock() {
-        var t = getTimeRemaining(endtime);
+        function updateClock() {
+            var t = getTimeRemaining(endtime);
 
-        if (daysSpan) daysSpan.innerHTML = t.days;
-        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+            if (daysSpan) daysSpan.innerHTML = t.days;
+            hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+            minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+            secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-        if (t.total <= 0) {
-          clearInterval(timeinterval);
+            if (t.total <= 0) {
+              clearInterval(timeinterval);
+            }
         }
+
+        updateClock();
+        var timeinterval = setInterval(updateClock, 1000);
     }
 
-    updateClock();
-    var timeinterval = setInterval(updateClock, 1000);
-}
+    var clock = makeClockOverlayDiv();
+    var timerLengthMinutes = window.startTimerMinutes || 15;
+    var deadline = new Date(Date.parse(new Date()) + timerLengthMinutes * 60 * 1000);
+    initializeClock(clock, deadline);
 
-var clock = makeClockOverlayDiv();
-var timerLengthMinutes = window.startTimerMinutes || 15;
-var deadline = new Date(Date.parse(new Date()) + timerLengthMinutes * 60 * 1000);
-initializeClock(clock, deadline);
+    var oldFullScreenElement = null;
 
-var oldFullScreenElement = null;
-
-document.addEventListener("fullscreenchange", function (event) {
-    if (document.fullscreenElement) {
-        console.log("Gone full screen", document.fullscreenElement);
-        var newClock = makeClockOverlayDiv(document.fullscreenElement);
-        initializeClock(newClock, deadline);
-        oldFullScreenElement = document.fullscreenElement;
-    } else {
-        console.log("Left full screen", document.fullscreenElement);
-        if (oldFullScreenElement)
-            deleteChildClocks(oldFullScreenElement);
-    }
-});
+    document.addEventListener("fullscreenchange", function (event) {
+        if (document.fullscreenElement) {
+            console.log("Gone full screen", document.fullscreenElement);
+            var newClock = makeClockOverlayDiv(document.fullscreenElement);
+            initializeClock(newClock, deadline);
+            oldFullScreenElement = document.fullscreenElement;
+        } else {
+            console.log("Left full screen", document.fullscreenElement);
+            if (oldFullScreenElement)
+                deleteChildClocks(oldFullScreenElement);
+        }
+    });
+})();
 
--- timer_starter.js
+++ timer_starter.js
@@ -1,8 +1,19 @@
-console.log("Hello");
-document.getElementById("startTimer").addEventListener("click", evt => {
-    console.log("Injecting");
-    var timerMinsElement = document.getElementById("timerMins");
-    var timerLen = timerMinsElement ? parseInt(timerMinsElement.value) : 10
-    chrome.tabs.executeScript({code:"window.startTimerMinutes = " + timerLen + ";"});
-    chrome.tabs.executeScript({file:"timer.js"});
-});
+(function () { 
+    var defaultTimerLen = 10;
+
+    chrome.storage.local.get(['lastTimerLen'], result => {
+        document.getElementById("timerMins").value = result.lastTimerLen || defaultTimerLen;
+    });
+
+    function getTimerLen() {
+        var timerMinsElement = document.getElementById("timerMins");
+        return timerMinsElement ? parseInt(timerMinsElement.value) : defaultTimerLen;
+    }
+
+    document.getElementById("startTimer").addEventListener("click", evt => {
+        var timerLen = getTimerLen();
+        chrome.tabs.executeScript({code:"window.startTimerMinutes = " + timerLen + ";"});
+        chrome.tabs.executeScript({file:"timer.js"});
+        chrome.storage.local.set({lastTimerLen: timerLen}, function () { window.close() });
+    });
+})();

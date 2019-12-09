e0317fec461d29020bb3b4ede36dfad708a215ad
Shaun Clowes <sclowes@mulesoft.com>
2019-12-09 07:57:52 -0800

    Start timer on enter

--- timer_starter.js
+++ timer_starter.js
@@ -1,6 +1,7 @@
 (function () { 
     var defaultTimerLen = 10;
 
+    // Set default value in text box based on last used timer length
     chrome.storage.local.get(['lastTimerLen'], result => {
         document.getElementById("timerMins").value = result.lastTimerLen || defaultTimerLen;
     });
@@ -10,10 +11,19 @@
         return timerMinsElement ? parseInt(timerMinsElement.value) : defaultTimerLen;
     }
 
-    document.getElementById("startTimer").addEventListener("click", evt => {
+    function startTimer() {
         var timerLen = getTimerLen();
         chrome.tabs.executeScript({code:"window.startTimerMinutes = " + timerLen + ";"});
         chrome.tabs.executeScript({file:"timer.js"});
         chrome.storage.local.set({lastTimerLen: timerLen}, function () { window.close() });
+    }
+
+    document.getElementById("startTimer").addEventListener("click", evt => {
+        startTimer();
+    });
+
+    document.getElementById("timerMins").addEventListener("keyup", evt => {
+        if (evt.keyCode == 13)
+            startTimer();
     });
 })();

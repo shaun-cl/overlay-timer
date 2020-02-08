73fdb216364cb0dacfe5b89e57a2549db178dc6f
Shaun Clowes <sclowes@mulesoft.com>
2020-02-08 13:35:39 -0800

    Play sounds at normalized volume

--- js/audio_content.js
+++ js/audio_content.js
@@ -43,29 +43,81 @@
     minPlayForSecs = minPlayForSecs || 0;
     volumePcnt = volumePcnt == undefined ? 1.0 : volumePcnt / 100.0;
 
-
     var sound      = document.createElement('audio');
     sound.id       = 'hiddenSoundPlayer';
     sound.src      = soundUrl;
     sound.type     = 'audio/mpeg';
     sound.volume   = volumePcnt;
-
-    oneTimeListener('canplaythrough', sound, evt => { 
-      var repeatTimes = Math.ceil(Math.max(0, minPlayForSecs / sound.duration - 1));
-      console.log('Audio loaded', evt.target, sound.duration, repeatTimes, minPlayForSecs);
-      sound.addEventListener('ended', evt => {
-        console.log('Sound ended');
-        if (repeatTimes-- <= 0) return;
-        sound.play();
-      });
-      console.log('Playing sound');
-      sound.play();
-    });
+    // sound.controls = true;
 
     var oldNode = document.getElementById(sound.id);
     if (oldNode)
       oldNode.parentNode.removeChild(oldNode);
     document.body.appendChild(sound);
+
+    // Normalize the volume of the mp3, from https://github.com/est31/js-audio-normalizer
+    var audioElem = sound;
+    var audioCtx = window.audioCtx || new AudioContext();
+    var src = audioCtx.createMediaElementSource(audioElem);
+    var gainNode = audioCtx.createGain();
+    gainNode.gain.value = 1.0;
+
+    audioElem.addEventListener("play", function() {
+      src.connect(gainNode);
+      gainNode.connect(audioCtx.destination);
+    }, true);
+
+    audioElem.addEventListener("pause", function() {
+      // disconnect the nodes on pause, otherwise all nodes always run
+      src.disconnect(gainNode);
+      gainNode.disconnect(audioCtx.destination);
+    }, true);
+
+    fetch(soundUrl)
+      .then(function(res) { return res.arrayBuffer(); })
+      .then(function(buf) {
+        return audioCtx.decodeAudioData(buf);
+      }).then(function(decodedData) {
+        var decodedBuffer = decodedData.getChannelData(0);
+        var sliceLen = Math.floor(decodedData.sampleRate * 0.05);
+        var averages = [];
+        var sum = 0.0;
+        for (var i = 0; i < decodedBuffer.length; i++) {
+          sum += decodedBuffer[i] ** 2;
+          if (i % sliceLen === 0) {
+            sum = Math.sqrt(sum / sliceLen);
+            averages.push(sum);
+            sum = 0;
+          }
+        }
+        // Ascending sort of the averages array
+        averages.sort(function(a, b) { return a - b; });
+        // Take the average at the 95th percentile
+        var a = averages[Math.floor(averages.length * 0.95)];
+
+        var gain = 1.0 / a;
+        // Perform some clamping
+        // gain = Math.max(gain, 0.02);
+        // gain = Math.min(gain, 100.0);
+
+        // ReplayGain uses pink noise for this one one but we just take
+        // some arbitrary value... we're no standard
+        // Important is only that we don't output on levels
+        // too different from other websites
+        gain = gain / 10.0;
+        console.log("Gain determined", name, a, gain);
+        gainNode.gain.value = gain;
+      });
+
+    var playedFor = 0;
+    sound.addEventListener('ended', evt => {
+      playedFor += evt.target.currentTime;
+      console.log('Sound ended', evt, playedFor);
+      if (playedFor >= minPlayForSecs)
+        return;
+      sound.play();
+    });
+    setTimeout(() => { console.log("Playing"); sound.play() }, 0);
   }
 
   console.log("Audio content loaded");
--- js/test.js
+++ js/test.js
@@ -1,7 +1,7 @@
 (function () {
   document.getElementById("playSound").addEventListener('click', evt => {
     var volumePcnt = document.getElementById("playVolume") && parseInt(document.getElementById("playVolume").value);
-    Audio.playRandomSound(2, false, volumePcnt).then(function (lastSoundUrl) {
+    Audio.playRandomSound(2, true, volumePcnt).then(function (lastSoundUrl) {
       var lastSoundUrlDiv = document.getElementById('lastSoundUrl');
       if (lastSoundUrlDiv)
         lastSoundUrlDiv.innerText = lastSoundUrl;

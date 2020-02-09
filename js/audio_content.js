5b601a701292d079060c99aa4f7649700687f26e
Shaun Clowes <sclowes@mulesoft.com>
2020-02-08 16:23:23 -0800

    Play all sounds on test page

--- js/audio_content.js
+++ js/audio_content.js
@@ -17,6 +17,8 @@
     return new Promise(resolve => chrome.runtime.sendMessage({command: 'getSeriousAudioFiles'}, resolve));
   }
 
+  Audio.getAllSounds = getAllSounds; 
+
   Audio.pickRandomSound = function (includePlayful) {
     return (includePlayful ? allSoundsPromise : seriousSoundsPromise)
             .then(sounds => chrome.runtime.getURL(sounds[Math.floor(Math.random()*sounds.length)]));
@@ -38,7 +40,7 @@
 
   console.log("Loading audio content");
 
-  Audio.playSound = function (soundUrl, minPlayForSecs, volumePcnt, autoPlay = true) {
+  Audio.playSound = function (soundUrl, minPlayForSecs, volumePcnt, autoPlay = true, normalizeVolume = true) {
     console.log("Playing sound", soundUrl);
     minPlayForSecs = minPlayForSecs || 0;
     volumePcnt = volumePcnt == undefined ? 1.0 : volumePcnt / 100.0;
@@ -55,60 +57,62 @@
       oldNode.parentNode.removeChild(oldNode);
     document.body.appendChild(sound);
 
-    // Normalize the volume of the mp3, from https://github.com/est31/js-audio-normalizer
-    var audioElem = sound;
-    var audioCtx = window.audioCtx || new AudioContext();
-    var src = audioCtx.createMediaElementSource(audioElem);
-    var gainNode = audioCtx.createGain();
-    gainNode.gain.value = 1.0;
+    if (normalizeVolume) {
+      // Normalize the volume of the mp3, from https://github.com/est31/js-audio-normalizer
+      var audioElem = sound;
+      var audioCtx = window.audioCtx || new AudioContext();
+      var src = audioCtx.createMediaElementSource(audioElem);
+      var gainNode = audioCtx.createGain();
+      gainNode.gain.value = 1.0;
 
-    audioElem.addEventListener("play", function (evt) {
-      console.log("Play", evt);
-      src.connect(gainNode);
-      gainNode.connect(audioCtx.destination);
-    }, true);
+      audioElem.addEventListener("play", function (evt) {
+        console.log("Normalize", evt);
+        src.connect(gainNode);
+        gainNode.connect(audioCtx.destination);
+      }, true);
 
-    audioElem.addEventListener("pause", function (evt) {
-      // disconnect the nodes on pause, otherwise all nodes always run
-      src.disconnect(gainNode);
-      gainNode.disconnect(audioCtx.destination);
-    }, true);
+      audioElem.addEventListener("pause", function (evt) {
+        // disconnect the nodes on pause, otherwise all nodes always run
+        src.disconnect(gainNode);
+        gainNode.disconnect(audioCtx.destination);
+      }, true);
 
-    fetch(soundUrl)
-      .then(function(res) { return res.arrayBuffer(); })
-      .then(function(buf) {
-        return audioCtx.decodeAudioData(buf);
-      }).then(function(decodedData) {
-        var decodedBuffer = decodedData.getChannelData(0);
-        var sliceLen = Math.floor(decodedData.sampleRate * 0.05);
-        var averages = [];
-        var sum = 0.0;
-        for (var i = 0; i < decodedBuffer.length; i++) {
-          sum += decodedBuffer[i] ** 2;
-          if (i % sliceLen === 0) {
-            sum = Math.sqrt(sum / sliceLen);
-            averages.push(sum);
-            sum = 0;
+      fetch(soundUrl)
+        .then(function(res) { return res.arrayBuffer(); })
+        .then(function(buf) {
+          return audioCtx.decodeAudioData(buf);
+        }).then(function(decodedData) {
+          var decodedBuffer = decodedData.getChannelData(0);
+          var sliceLen = Math.floor(decodedData.sampleRate * 0.05);
+          var averages = [];
+          var sum = 0.0;
+          for (var i = 0; i < decodedBuffer.length; i++) {
+            sum += decodedBuffer[i] ** 2;
+            if (i % sliceLen === 0) {
+              sum = Math.sqrt(sum / sliceLen);
+              averages.push(sum);
+              sum = 0;
+            }
           }
-        }
-        // Ascending sort of the averages array
-        averages.sort(function(a, b) { return a - b; });
-        // Take the average at the 95th percentile
-        var a = averages[Math.floor(averages.length * 0.95)];
+          // Ascending sort of the averages array
+          averages.sort(function(a, b) { return a - b; });
+          // Take the average at the 95th percentile
+          var a = averages[Math.floor(averages.length * 0.95)];
 
-        var gain = 1.0 / a;
-        // Perform some clamping
-        // gain = Math.max(gain, 0.02);
-        // gain = Math.min(gain, 100.0);
+          var gain = 1.0 / a;
+          // Perform some clamping
+          // gain = Math.max(gain, 0.02);
+          // gain = Math.min(gain, 100.0);
 
-        // ReplayGain uses pink noise for this one one but we just take
-        // some arbitrary value... we're no standard
-        // Important is only that we don't output on levels
-        // too different from other websites
-        gain = gain / 10.0;
-        console.log("Gain determined", name, a, gain);
-        gainNode.gain.value = gain;
-      });
+          // ReplayGain uses pink noise for this one one but we just take
+          // some arbitrary value... we're no standard
+          // Important is only that we don't output on levels
+          // too different from other websites
+          gain = gain / 10.0;
+          console.log("Gain determined", name, a, gain);
+          gainNode.gain.value = gain;
+        });
+    }
 
     var playedFor = 0;
     sound.addEventListener('ended', evt => {
--- js/test.js
+++ js/test.js
@@ -1,12 +1,19 @@
 (function () {
+  var getVolumePcnt = () => document.getElementById("playVolume") && parseInt(document.getElementById("playVolume").value);
+  var setLastSound = (url) => (document.getElementById('lastSoundUrl') || {}).innerText = url;
+
   document.getElementById("playSound").addEventListener('click', evt => {
-    var volumePcnt = document.getElementById("playVolume") && parseInt(document.getElementById("playVolume").value);
-    Audio.playRandomSound(2, true, volumePcnt).then(function (lastSoundUrl) {
-      var lastSoundUrlDiv = document.getElementById('lastSoundUrl');
-      if (lastSoundUrlDiv)
-        lastSoundUrlDiv.innerText = lastSoundUrl;
-    });
+    Audio.playRandomSound(2, true, getVolumePcnt()).then(lastSoundUrl => setLastSound(lastSoundUrl));
   });
+
+  var allSoundsDiv = document.getElementById("listAllSounds");
+  Audio.getAllSounds().then(soundUrls => soundUrls.forEach(soundUrl => {
+    var newDiv = document.createElement("div");
+    newDiv.innerHTML = "Sound: " + soundUrl + " <button class='original'>Play</button><button class='normalized'>Play Normalized</button>";
+    newDiv.querySelector("button.original").addEventListener("click", () => Audio.playSound(chrome.runtime.getURL(soundUrl), undefined, undefined, true, false));
+    newDiv.querySelector("button.normalized").addEventListener("click", () => Audio.playSound(chrome.runtime.getURL(soundUrl), undefined, undefined, true, true));
+    allSoundsDiv.appendChild(newDiv);
+  }));
 
   if (startTimer) 
     startTimer(20);
